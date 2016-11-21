fbda319a33a96e4e5221c613e3d015520c2be273
Tim Officer <timothyofficer@gmail.com>
2016-11-21 09:56:10 -0500

    styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,7 @@
 'use strict';
 
 const gulp = require('gulp');
-const less = require('gulp-less');
-const LessPrefix = require('less-plugin-autoprefix');
 
-const LESSOPTIONS = {
-  plugins: [
-    new LessPrefix({
-      browsers: "> 1%, last 2 versions, Firefox ESR"
-    })
-  ]
-};
-
-/**
- * @desc Compile .less files to .css
- */
-gulp.task('css', () => {
-  return gulp.src('src/less/main.less')
-    .pipe(less(LESSOPTIONS))
-    .pipe(gulp.dest('src/css'));
-});
-
-gulp.task('server', ['css'], () => {
-  gulp.watch('src/less/**/*.less', ['css'])
-
+gulp.task('server', [], () => {
   require('./server.js');
 });
