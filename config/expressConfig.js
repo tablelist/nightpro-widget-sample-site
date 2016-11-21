0aed5f73b0a18cfec0a06b0cf572673ff8d89ee5
Tim Officer <timothyofficer@gmail.com>
2016-11-21 10:10:13 -0500

    add events page

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 
 app.use('/index.html', express.static('./index.html'));
+app.use('/event-detail.html', express.static('./event-detail.html'));
+app.use('/events.html', express.static('./events.html'));
 app.use('/src', express.static('./src'));
 
 module.exports = app;
