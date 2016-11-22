222c53ec1ed46349cbc3e791895a3d5bca235c41
Tim Officer <timothyofficer@gmail.com>
2016-11-22 11:37:24 -0500

    new pages

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -7,6 +7,7 @@ const app = express();
 app.use('/index.html', express.static('./index.html'));
 app.use('/event-detail.html', express.static('./event-detail.html'));
 app.use('/events.html', express.static('./events.html'));
+app.use('/reservations.html', express.static('./reservations.html'));
 app.use('/src', express.static('./src'));
 
 module.exports = app;
