68f05da4fd03cec12ca2bfb5afd69332a30126e1
Alex Johnson <alex@tablelist.com>
2016-11-22 14:39:47 -0500

    Add Buy Now button to footer. Temp /tablelist.html. Cleanup hover effects

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -8,6 +8,7 @@ app.use('/index.html', express.static('./index.html'));
 app.use('/event-detail.html', express.static('./event-detail.html'));
 app.use('/events.html', express.static('./events.html'));
 app.use('/reservations.html', express.static('./reservations.html'));
+app.use('/tablelist.html', express.static('./tablelist.html'));
 app.use('/src', express.static('./src'));
 
 module.exports = app;
