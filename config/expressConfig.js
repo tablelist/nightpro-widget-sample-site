3c7f18017a06b7b752e482312ced9c27c2a3932a
Tim Officer <timothyofficer@gmail.com>
2017-02-21 14:00:34 -0500

    add a bottle service page with the Tablelist widget

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -8,6 +8,7 @@ app.use('/index.html', express.static('./index.html'));
 app.use('/event-detail.html', express.static('./event-detail.html'));
 app.use('/events.html', express.static('./events.html'));
 app.use('/reservations.html', express.static('./reservations.html'));
+app.use('/bottleService.html', express.static('./bottleService.html'));
 app.use('/src', express.static('./src'));
 
 module.exports = app;
